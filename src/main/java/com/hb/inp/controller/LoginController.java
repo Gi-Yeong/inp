d19bf8af85aa65ea6926819feb4b3d81e0181e97
Gi-Yeong <kiyoung85@gmail.com>
2016-12-23 12:30:42 +0900

    Login with Oracle, return key db_mySQL

--- src/main/java/com/hb/inp/controller/LoginController.java
+++ src/main/java/com/hb/inp/controller/LoginController.java
@@ -2,15 +2,18 @@ package com.hb.inp.controller;
 
 import com.hb.inp.model.Login.LoginDao;
 import org.apache.ibatis.session.SqlSession;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/login/")
@@ -19,7 +22,7 @@ public class LoginController {
     @Autowired
     private SqlSession oracleSqlSession;
 
-    @RequestMapping(value = "/memberlogin", method = RequestMethod.POST)
+    @RequestMapping(value = "/memberlogin")
     public void guestLogin(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("start");
         String id = request.getParameter("id");
@@ -36,9 +39,30 @@ public class LoginController {
 
         String db_info = oracleSqlSession.getMapper(LoginDao.class).logincheck(id, pw);
 
-        System.out.println("got db_key");
+        System.out.println("JSON OBJECT MAKE");
+        Map<String, Object> jsonObject = new HashMap<String, Object>();
+        Map<String, Object> jsonSubObject = null;
+        ArrayList<Map<String, Object>> jsonList = new ArrayList<Map<String, Object>>();
+        String callBack = request.getParameter("callback");
+
+        jsonSubObject = new HashMap<String, Object>();
+        jsonSubObject.put("db_key_from_Oracle", db_info);
+        jsonList.add(jsonSubObject);
+        jsonObject.put("success", true);
+        jsonObject.put("total_count", 1);
+        jsonObject.put("result_list", jsonList);
+        System.out.println(jsonList.toString());
+
+        String result = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            result = mapper.writeValueAsString(jsonObject);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(callBack + "(" + result + ")");
         assert out != null;
-        out.print(db_info);
+        out.write(callBack + "(" + result + ")");
         out.flush();
         out.close();
         System.out.println("login success");
